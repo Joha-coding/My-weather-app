8bd511f5dd6586d8762cc2c47bf171e253e8b68e
Joha <jotz@outlook.fr>
2023-11-30 16:28:41 +0100

    injected weather-images from the API

--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@ function refreshWeather(response) {
   let windElement = document.querySelector("#wind");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon-weather");
+
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class=icon-weather"/>`;
 
   cityElement.innerHTML = response.data.city;
   timeElement.innerHTML = formatDate(date);
