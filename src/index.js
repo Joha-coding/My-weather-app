eec9e4e9f3061c48e593e4a5cb5780d31086073d
Joha <jotz@outlook.fr>
2023-12-01 12:41:02 +0100

    Inject the forecast HTML from JS
    - removed html from html to inject it from JS
    - created the displayForecast function  and created an array of 5 days
    - used a loop to call days 1 by 1 via JS

--- src/index.js
+++ src/index.js
@@ -51,7 +51,30 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  let days = ["Tue", "Wed", "Thur", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+        <div class="weather-forecast-day">          
+    <div class="weather-forecast-date"><strong>${day}</strong></div>
+ <div class="weather-forecast-icon"><strong>☀️</strong></div>
+ <div class="weather-forecast-temperatures">
+  <span class="weather-forecast-temperature-max"><strong>18º</strong></span
+  >/<span class="weather-forecast-temperature-min">2º</span>
+    </div>
+        </div>
+`;
+  });
+
+  forecastElement.innerHTML = forecastHtml;
+}
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("New York");
+displayForecast();
